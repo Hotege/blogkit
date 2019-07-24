278cd5a359adf223d01d948ad7ae256c8d4e34ab
DL <myownroc@163.com>
2019-07-24 15:59:18 +0800

    update to version 1.2.0

--- build.sh
+++ build.sh
@@ -7,5 +7,5 @@ go build -o working/blogkit -a -ldflags '-s -w -linkmode "external" -extldflags 
 
 upx working/blogkit -9 -q > /dev/null
 
-docker rmi blogkit:1.1.1
-docker build -t blogkit:1.1.1 .
+docker rmi blogkit:1.2.0
+docker build -t blogkit:1.2.0 .
--- test.sh
+++ test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --rm --name blogkit -v `pwd`/working:/blogkit -p 80:80 blogkit:1.1.1
+docker run --rm --name blogkit -v `pwd`/working:/blogkit -p 80:80 blogkit:1.2.0
