fcbb8adbd86b4f380de4901f96bc3e859e6160b8
DL <myownroc@163.com>
2019-08-01 16:34:10 +0800

    update to version 1.2.5

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ go build -o working/blogkit -a -ldflags '-s -w -linkmode "external" -extldflags 
 upx working/blogkit -9 -q > /dev/null
 
 # remove old docker image
-docker rmi blogkit:1.2.4
+docker rmi blogkit:1.2.5
 
 # build docker image
-docker build -t blogkit:1.2.4 .
+docker build -t blogkit:1.2.5 .
